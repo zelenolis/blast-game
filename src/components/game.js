0115dc0ebd7f1af5a2fd207562b52cbeb61ebd95
Eugene <josumkin@yahoo.com>
2024-10-22 18:01:38 +0400

    feat: add gameField init array

---
+++ src/components/game.js
@@ -0,0 +1,23 @@
+import { filedX, filedY, colours } from '../constants.js'
+
+const field = [];
+
+class Tile {
+    constructor(x, y, color) {
+        this.x = x
+        this.y = y
+        this.color = color
+    }
+}
+
+function fieldInit() {
+    for (let i = 0; i < filedX; i++) {
+        for (let j = 0; j < filedY; j++) {
+            field.push(new Tile(i, j, randomColor()))
+        }
+    }
+}
+
+function randomColor() {
+    return 1 + Math.floor(Math.random() * colours)
+}
