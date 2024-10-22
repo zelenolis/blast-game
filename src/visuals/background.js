56fe4d8d537b161e4ac9e6d96da20ab13b1644c9
Eugene <josumkin@yahoo.com>
2024-10-22 11:56:11 +0400

    feat: add header rect draw

--- src/visuals/background.js
+++ src/visuals/background.js
@@ -1,13 +1,25 @@
 import { canvas, ctx, canvasW, canvasH } from "./welcome.js";
-import { fieldPos } from "../utils/positions.js";
-import { darkColor } from "../constants.js";
+import { fieldPos, levelPos, currentScorePos, totalScorePos } from "../utils/positions.js";
+import { darkColor, baseColor } from "../constants.js";
 
 
-function levelDraw() {}
+function levelDraw() {
+    const coords = levelPos()
+    ctx.fillStyle = 'red'
+    ctx.fillRect(coords[0], coords[1], coords[2], coords[3])
+}
 
-function currentScoreDraw() {}
+function currentScoreDraw() {
+    const coords = currentScorePos()
+    ctx.fillStyle = 'green'
+    ctx.fillRect(coords[0], coords[1], coords[2], coords[3])
+}
 
-function totalScoreDraw() {}
+function totalScoreDraw() {
+    const coords = totalScorePos()
+    ctx.fillStyle = 'blue'
+    ctx.fillRect(coords[0], coords[1], coords[2], coords[3])
+}
 
 export function fieldDraw() {
     const coords = fieldPos()
