5c9e02b22bce6f519950bb895501a9d7dd1a5b13
Eugene <josumkin@yahoo.com>
2024-10-22 19:08:44 +0400

    feat: add click catcher

--- src/main.js
+++ src/main.js
@@ -3,10 +3,29 @@ import { startScreen, canvas, clearCanvas } from './visuals/welcome.js'
 import { fieldDraw } from './visuals/background.js'
 import { fieldInit } from './components/game.js'
 
+// game status: init - start screen, game - game is on
+let gameStatus = 'init'
+
 startScreen()
 fieldInit()
 
+
 canvas.addEventListener('click', function(e) {
-    clearCanvas()
-    fieldDraw()
+
+    switch (gameStatus) {
+        case 'init':
+            clearCanvas()
+            fieldDraw()
+            gameStatus = 'game'
+            break
+        case 'game':
+            clickCoords(e)
+    }
 })
+
+function clickCoords(e) {
+    const rect = canvas.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+    console.log("x: " + x + " y: " + y)
+}
