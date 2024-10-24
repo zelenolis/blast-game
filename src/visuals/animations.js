5a675b5009e4ed33fd9f5be0dea776e311fc744d
Eugene <josumkin@yahoo.com>
2024-10-24 13:15:52 +0400

    refactor: basic animations

--- src/visuals/animations.js
+++ src/visuals/animations.js
@@ -1,5 +1,5 @@
 import { ctx } from "./welcome.js"
-import { darkColor } from "../constants"
+import { darkColor } from "../constants.js"
 import Ghost from '../assets/ghost.png'
 
 
@@ -11,23 +11,11 @@ export function tileDestroy(x, y, length) {
     animationImage.src = Ghost
 
     animationImage.onload = function() {
-        ghostDisappear(x, y, length, animationImage)
+        ctx.drawImage(animationImage, x, y, length, length)
     }
 }
 
 function clearTyle(x, y, length) {
     ctx.fillStyle = darkColor
-    ctx.fillRect(x, y, x + length, y + length)
-}
-
-function ghostDisappear(x, y, length, animationImage) {
-    let opacity = 1
-    let speed = 0.01
-
-    while (opacity > 0) {
-        ctx.drawImage(animationImage, x, y, x + length, y + length)
-        opacity -= speed
-        ctx.globalAlpha = opacity
-    }
-    ctx.globalAlpha = 1
+    ctx.fillRect(x, y, length, length)
 }
