56263ba7da07096bc334768a8a1edd29c380cc74
Eugene <josumkin@yahoo.com>
2024-10-25 17:32:04 +0400

    feat: change game field

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,8 @@ export const lightColor = 'white'
 export const baseColor = '#C76E00'
 
 // Field size X * Y
-export const filedX = 10
-export const filedY = 10
+export const filedX = 8
+export const filedY = 8
 
 // Number of color variations
 export const colours = 5
