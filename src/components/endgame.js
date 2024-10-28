c47bfe47ad540899347f81eef01c68f4a1658dc0
Eugene <josumkin@yahoo.com>
2024-10-28 15:40:24 +0400

    feat: add remix colors template functions

--- src/components/endgame.js
+++ src/components/endgame.js
@@ -1,4 +1,15 @@
+import { remixField } from "./game.js";
+import { remixes } from "../constants.js";
 
+let remixRemains = 0
+
+export function resetRemixes() {
+    remixRemains = remixes
+}
+
+function decreaseRemixes() {
+    remixRemains -= 1
+}
 
 function checkEndGame() {
     //
