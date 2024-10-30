3241258fe6a2f7a29157f9fac742164ac42b08e6
Eugene <josumkin@yahoo.com>
2024-10-30 15:13:20 +0400

    feat :add start audio

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,10 +1,12 @@
 import Pop from '../assets/pop.mp3'
 import Click from '../assets/click.mp3'
 import Miss from '../assets/miss.mp3'
+import Start from '../assets/Halloween-song.mp3'
 
 const popSound = new Audio(Pop)
 const clickSound = new Audio(Click)
 const missSound = new Audio(Miss)
+const startSound = new Audio(Start)
 
 export function playPop() {
     popSound.play()
@@ -16,4 +18,8 @@ export function playClick() {
 
 export function playMiss() {
     missSound.play()
+}
+
+export async function playStart() {
+    startSound.play();
 }
