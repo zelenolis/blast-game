c6e60ed74566b3e0b101f86be0bbb4bb79d27cc2
Eugene <josumkin@yahoo.com>
2024-10-31 17:42:21 +0400

    chores: remove unused func

--- src/utils/resize.js
+++ src/utils/resize.js
@@ -1,16 +1,3 @@
-export function startLogoSize (screenWidth, logoWidth, logoHeight) {
-    let w = 0
-    let h = 0
-    if (logoWidth * 2 > screenWidth) {
-        w = screenWidth / 2
-        h = logoHeight / (logoWidth / w)
-    } else {
-        w = logoWidth
-        h = logoHeight
-    }
-    return [w, h]
-}
-
 export function startFontSize (screenWidth) {
     const maxEnlarge = 700
     const maxFontSize = 70
